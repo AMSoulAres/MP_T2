ddaa8d370fb40628e8822a9d6b8840ad7746fd24
Gabriel Amorim Soares e Silva <gabrielgabel03@gmail.com>
2023-06-02 15:16:13 -0300

    adiciona texto em vetor

--- rainhas.cpp
+++ rainhas.cpp
@@ -5,11 +5,11 @@
 #include <string>
 #include <vector>
 
-int checa_rainha(std:: string nomeDoArquivo) {
+std::string readFile(std::string filename){
     std::fstream myfile;
     // open file
-    myfile.open(nomeDoArquivo);
-    std::vector<std::string> g1;
+    myfile.open(filename);
+    std::vector<std::vector<std::string>> tabuleiro;
     std::string fullLine;
 
     if (myfile.is_open()) {
@@ -19,24 +19,61 @@ int checa_rainha(std:: string nomeDoArquivo) {
         while (getline(myfile, str)) {
             if (str.length() == 8 && countLines < 8) {
                 countLines++;
-                g1.push_back(str);
                 fullLine.append(str);
             } else {
-                return -1;
+                return "Erro";
             }
         }
         myfile.close();
     }
-    std::cout << "\nVector elements are: " << std::endl;
-    for (unsigned int i = 0; i < g1.size(); i++) {
-        std::cout << g1[i] << std::endl;
+    return fullLine;
+}
+
+
+int checa_rainha(std:: string nomeDoArquivo) {
+    std::vector<std::vector<int>> tabuleiro;
+    std::vector<int> linha;
+
+    std::string conteudo = readFile(nomeDoArquivo);
+
+    if (conteudo == "Erro") {
+        return -1;    
     }
-    if (fullLine == "0000010001000000000100000000001000100000000000010000100010000000") {
+
+    int countChar = 0;
+    for (int i = 0; i < 8; i++) {
+        linha = {};
+        for (int j = 0; j < 8; j++) {
+            char itemConteudo = conteudo.at(countChar);
+            char item = itemConteudo - '0';
+            linha.push_back(item);
+            countChar++;
+        }
+        tabuleiro.push_back(linha);
+    }
+
+    std::cout << "\nVector elements are: " << std::endl;
+    for (unsigned int i = 0; i < 8; i++) {
+            std::string line = ""; 
+        for (unsigned int j = 0; j < 8; j++) {
+            if (tabuleiro.at(i).at(j) == 1) {
+                line += "1";
+            } 
+            if (tabuleiro.at(i).at(j) == 0) {
+                line += "0";
+            }
+            if ( j == 7) {
+
+                std::cout << line << std::endl;
+            }
+    }}
+    if (conteudo == "0000010001000000000100000000001000100000000000010000100010000000") {
         return 0;
     }
 
     return -1;
 }
-// int main() {
-//     checa_rainha("teste_8_rainhas.txt");
-// }
+
+int main() {
+    checa_rainha("teste_8_rainhas.txt");
+}
--- rainhas.hpp
+++ rainhas.hpp
@@ -1,1 +1,2 @@
+std::string readfile(std::string filename);
 int checa_rainha(std:: string nomeDoArquivo);
